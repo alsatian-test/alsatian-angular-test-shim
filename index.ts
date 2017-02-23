bd68482ce5d0c6f68843ecfce96b7e2eb83fb787
jamesrichford <jamesrichford@googlemail.com>
2017-02-23 12:43:26 +0000

    add dependencies

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@ang
 
 const document = jsdom("<!doctype html><html><body></body></html>");
 
-const window = document.defaultView;
+const window = document.parentWindow;
 
 (global as any).document = document;
 (global as any).HTMLElement = (window as any).HTMLElement;
