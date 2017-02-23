63401e8a1d105af1032d886369503a9383a10593
jamesrichford <jamesrichford@googlemail.com>
2017-02-23 12:37:54 +0000

    add shim

---
+++ index.ts
@@ -0,0 +1,14 @@
+import "zone.js";
+import { jsdom } from "jsdom";
+import { TestBed } from "@angular/core/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+
+const document = jsdom("<!doctype html><html><body></body></html>");
+
+const window = document.defaultView;
+
+(global as any).document = document;
+(global as any).HTMLElement = (window as any).HTMLElement;
+(global as any).XMLHttpRequest = (window as any).XMLHttpRequest;
+
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
